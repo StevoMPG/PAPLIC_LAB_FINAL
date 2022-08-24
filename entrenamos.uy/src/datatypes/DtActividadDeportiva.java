89a8f08a433a7ab807d2d239d5c01dbf2e45f843
StevoMPG <steven.pereira0496@gmail.com>
2022-08-24 10:23:30 -0300

    Agregado DtActDep

--- entrenamos.uy/src/datatypes/DtActividadDeportiva.java
+++ entrenamos.uy/src/datatypes/DtActividadDeportiva.java
@@ -1,5 +1,39 @@
 package datatypes;
 
 public class DtActividadDeportiva {
+	
+	private String nombre;
+	private String descripcion;
+	private int duracionMinutos;
+	private float costo;
+	private DtFechaHora fechaRegistro;
+
+	public DtActividadDeportiva(String nom, String desc, int dur, float cos, DtFechaHora fech){
+		this.nombre = nom;
+		this.descripcion = desc;
+		this.duracionMinutos = dur;
+		this.costo = cos;
+		this.fechaRegistro = fech;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+	
+	public int getDuracionMinutos() {
+		return this.duracionMinutos;
+	}
+	
+	public float getCosto() {
+		return this.costo;
+	}
+	
+	public DtFechaHora getFechaRegistro() {
+		return this.fechaRegistro;
+	}
 
 }
