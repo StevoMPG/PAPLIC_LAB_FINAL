6f904f7d70719531b4cbac8d71f935c382891c91
StevoMPG <steven.pereira0496@gmail.com>
2022-08-24 10:28:45 -0300

    Me aburri, luego sigo haciendo los Dt xd

--- entrenamos.uy/src/datatypes/DtClase.java
+++ entrenamos.uy/src/datatypes/DtClase.java
@@ -2,4 +2,46 @@ package datatypes;
 
 public class DtClase {
 
+private String nombre, correoProfesor, nicknameProfesor, URL;
+	
+	private int minSocios, maxSocios;
+	
+	private DtFechaHora fechaClase, fechaRegistro;	
+	
+	public DtClase(String nom, String nickP, String emailP, int min, int max, String url, DtFechaHora fechC, DtFechaHora fechR){
+		nombre = nom;
+		correoProfesor = emailP;
+		nicknameProfesor = nickP;
+		minSocios = min;
+		maxSocios = max;
+		URL = url;
+		fechaClase = fechC;
+		fechaRegistro = fechR;
+	}
+	
+	public String getNombre() { return nombre; }
+	
+	public String getNicknameProfesor() { return nicknameProfesor; }
+	
+	public String getCorreoProfesor() { return correoProfesor; }
+	
+	public int getMinSocios() {
+		return this.minSocios;
+	}
+	
+	public int getMaxSocios() {
+		return this.maxSocios;
+	}
+	
+	public String getURL() {
+		return this.URL;
+	}
+	
+	public DtFechaHora getFechaClase() {
+		return this.fechaClase;
+	}
+	
+	public DtFechaHora getFechaRegistro() {
+		return this.fechaRegistro;
+	}
 }
--- entrenamos.uy/src/datatypes/DtClasesCuponeras.java
+++ entrenamos.uy/src/datatypes/DtClasesCuponeras.java
@@ -1,5 +1,20 @@
 package datatypes;
 
 public class DtClasesCuponeras {
+	
+	private String nombreActividad;
+	
+	private int cantidadClases;
+	
+	public DtClasesCuponeras(String s, int t) {
+		nombreActividad = s;
+		cantidadClases = t;
+	}
+	public String getNombreActividad() {
+		return nombreActividad;
+	}
+	public int getCantidadClases() {
+		return cantidadClases;
+	}
 
 }
