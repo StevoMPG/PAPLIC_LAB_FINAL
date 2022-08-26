935ad826054ddec1b2173361bbae03cd71fc451e
StevoMPG <steven.pereira0496@gmail.com>
2022-08-26 20:31:35 -0300

    Agregados comentarios en el manejador Usuario

--- entrenamos.uy/src/logica/manejadorUsuario.java
+++ entrenamos.uy/src/logica/manejadorUsuario.java
@@ -1,5 +1,12 @@
 package logica;
 
+import java.util.Set;
+
 public class manejadorUsuario {
 
+	private static manejadorUsuario instancia = null;
+	
 }
+
+
+// Aca tiene que ir agregarusuario, encontrausuario, el existe nick, correo y obtener el nickname
